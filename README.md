8f15921bb0e22a4685996a3df566e4a6f26415eb
Vanessa Oliveira <138900104+devnessa@users.noreply.github.com>
2023-10-04 21:50:40 -0300

    Add files via upload
    Segundo commit com todas as informações do portfólio que faltavam.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # devnessa.github.io
 Meu portfólio / My portfolio.
+
+Primeiro portfólio desenvolvido utilizando HTML5, CSS e JavaScript.
---
+++ app.js
@@ -0,0 +1,69 @@
+class FormSubmit {
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
+    }
+  
+    displaySuccess() {
+      this.form.innerHTML = this.settings.success;
+    }
+  
+    displayError() {
+      this.form.innerHTML = this.settings.error;
+    }
+  
+    getFormObject() {
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+      });
+      formObject["to"]={"email": "nessa7542@gmail.com", "name": "Vanessa"};
+      console.log(formObject);
+      return formObject;
+    }
+  
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+      event.target.innerText = "Enviando...";
+    }
+  
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Basic d98b1f0290887c810d0471bfd5629063:44b67de9a3c3c14bfae22b5fc420e9f4",
+          },
+          body: JSON.stringify(this.getFormObject()),
+          auth: {username: 'd98b1f0290887c810d0471bfd5629063', password: '44b67de9a3c3c14bfae22b5fc420e9f4'}
+        });
+        this.displaySuccess();
+      } catch (error) {
+        this.displayError();
+        throw new Error(error);
+      }
+    }
+  
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
+    }
+  }
+  
+  const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Mensagem enviada!</h1>",
+  });
+  formSubmit.init();
